cb2737d192584741f4542f465872ee25cea6a427
Daniel Park <parkant@bc.edu>
2022-01-30 16:32:58 -0500

    ch 2.8

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -16,12 +16,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         messageLabel.text = ""
     }
-
-
+    
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         print("😂 The message button was pressed!")
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "img0")
+        let awesomeMessage = "You are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You are Da Bomb!"
+        if messageLabel.text == awesomeMessage{
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "img1")
+        } else if messageLabel.text == greatMessage{
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "img2")
+        }
+        else{
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "img0")
+        }
     }
 }
 
